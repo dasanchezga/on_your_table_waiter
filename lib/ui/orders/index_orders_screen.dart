c8aae7ff1d88e56f3c307f6a4a3b801ad0b59a19
dasanchezga <dasanchezga@unal.edu.co>
2022-10-29 17:36:41 -0500

    orders in progress

--- lib/ui/orders/index_orders_screen.dart
+++ lib/ui/orders/index_orders_screen.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:on_your_table_waiter/features/auth/provider/auth_provider.dart';
-import 'package:on_your_table_waiter/features/restaurant/provider/restaurant_provider.dart';
-import 'package:on_your_table_waiter/features/table/provider/table_provider.dart';
-import 'package:on_your_table_waiter/ui/menu/help_menu_screen.dart';
 import 'package:on_your_table_waiter/ui/menu/menu_screen.dart';
 import 'package:on_your_table_waiter/ui/orders/orders_screen.dart';
-import 'package:on_your_table_waiter/ui/menu/table_menu_screen.dart';
 import 'package:on_your_table_waiter/ui/orders/tables_screen.dart';
-import 'package:on_your_table_waiter/ui/widgets/bottom_sheet/not_authenticated_bottom_sheet.dart';
 
 class IndexOrdersScreen extends ConsumerStatefulWidget {
   const IndexOrdersScreen({super.key});
--- lib/ui/orders/orders_screen.dart
+++ lib/ui/orders/orders_screen.dart
@@ -14,104 +14,19 @@ class _OrdersScreenState extends ConsumerState<OrdersScreen> {
   
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          pinned: true,
-          expandedHeight: 200,
-          systemOverlayStyle: SystemUiOverlayStyle.light,
-          backgroundColor: Colors.grey,
-          flexibleSpace: FlexibleSpaceBar(
-              titlePadding: const EdgeInsets.all(16),
-              background: Stack(
-                fit: StackFit.expand,
-                children: [
-                  //ImageFiltered(
-                    //imageFilter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
-                    //child: data.imageUrl == null
-                        //? const FlutterLogo()
-                        //: Image.network(
-                          //  data.imageUrl!,
-                           // fit: BoxFit.cover,
-                          //),
-                  //),
-                  Container(color: Colors.black.withOpacity(0.2)),
-                ],
-              ),
-              title: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  //data.logoUrl == null
-                    //  ? const FlutterLogo()
-                      //: Image.network(
-                        //  data.logoUrl!,
-                          //height: 50,
-                          //width: 50,
-                        //),
-                  const SizedBox(width: 5),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'PEDIDOS',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-        ),
-        SliverToBoxAdapter(
-            child: Container(
-              margin: const EdgeInsets.symmetric(vertical: 20),
-              height: 10,
-              alignment: Alignment.center,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  //final cat = data.categories[index];
-                  //final isSelected = selectedCategoryIndex == index;
-                  return Padding(
-                    padding: EdgeInsets.only(
-                      right: 15,
-                      top: 2,
-                      bottom: 2,
-                      left: index == 0 ? 10 : 0,
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index){
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                     child: Container(
-                       color:
-                           index % 2 == 0 ? Colors.grey : Colors.white30,
-                       height: 80,
-                       alignment: Alignment.center,
-                       child: Text(
-                         "Item $index",
-                         style: const TextStyle(fontSize: 30),
-                       ),
-                     ),
-                );
-              },
-              childCount: 8, 
-            ),
-          ),
-         const SliverToBoxAdapter(child: SizedBox(height: 60)),
-      ],
-    );          
+    return Scaffold(
+      appBar: AppBar(title: const Text('PEDIDOS')),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: 9,
+        itemBuilder:(context, index){
+          return Container(
+            height: 50,
+            color: Colors.grey,
+            child: Center(child: Text('Pedido $index'))
+          );
+        } 
+      )  //=> Text('Item $index') ),
+    );     
   }
-
 }
