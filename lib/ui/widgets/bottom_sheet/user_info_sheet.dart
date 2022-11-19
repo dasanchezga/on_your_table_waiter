a2cc260c253679a8203aefc173265c5f6f7e660e
jubedoyat <jubedoyat@unal.edu.co>
2022-11-19 14:31:09 -0500

    Ahora sí funciona xd

--- lib/ui/widgets/bottom_sheet/user_info_sheet.dart
+++ lib/ui/widgets/bottom_sheet/user_info_sheet.dart
@@ -33,34 +33,40 @@ class _UserInfoSheet extends ConsumerState<UserInfoSheet> {
   Widget build(BuildContext context) {
     final authState = ref.read(authProvider);
     return BaseBottomSheet(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(onPressed: Navigator.of(context).pop, icon: const Icon(Icons.close)),
-            Flexible(
-              child: authState.authModel.on(onData: (data) => Text(
-                '${data.user.firstName} ${data.user.lastName}',
-                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              onError: (e) => Center(
-                  child: Text(e.message),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: authState.authModel.on(
+                  onData: (data) => Text(
+                    '${data.user.firstName} ${data.user.lastName}',
+                    style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  onError: (e) => Center(
+                    child: Text(e.message),
+                  ),
+                  onInitial: () => const Center(child: CircularProgressIndicator()),
+                  onLoading: () => const Center(child: CircularProgressIndicator()),
                 ),
-              onInitial: () => const Center(child: CircularProgressIndicator()),
-              onLoading: () => const Center(child: CircularProgressIndicator()),
-            ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 25,
-              width: double.infinity,
-              child: CustomElevatedButton(onPressed: onLogout, child: const Text('Cerrar sesión')),
-            ),
-          ],
-        ),
-      );
+              ),
+              IconButton(onPressed: Navigator.of(context).pop, icon: const Icon(Icons.close)),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 25,
+            width: double.infinity,
+            child: CustomElevatedButton(onPressed: onLogout, child: const Text('Cerrar sesión')),
+          ),
+        ],
+      ),
+    );
   }
 
   void onLogout() {
