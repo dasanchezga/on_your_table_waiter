ccdb02d9f937bff9c142bb221c39f3878db50e20
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-11-28 02:23:49 -0500

    Added aws env

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,6 +8,15 @@ import 'package:on_your_table_waiter/app.dart';
 
 void main() async {
   EquatableConfig.stringify = true;
+  HttpOverrides.global = MyHttpOverrides();
   await Hive.initFlutter();
   runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
 }
