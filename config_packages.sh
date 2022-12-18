5320153cc2766bdbdefac4bf0b9e1c35adf9204f
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-18 14:14:05 -0500

    Removed flag

--- config_packages.sh
+++ config_packages.sh
@@ -1,11 +1,11 @@
 cd packages
 git submodule add https://github.com/nestorsgarzonc/oyt_front_menu
-git submodule add --force https://github.com/nestorsgarzonc/oyt_front_auth
-git submodule add --force https://github.com/nestorsgarzonc/oyt_front_core
-git submodule add --force https://github.com/nestorsgarzonc/oyt_front_order
-git submodule add --force https://github.com/nestorsgarzonc/oyt_front_orders_queue
-git submodule add --force https://github.com/nestorsgarzonc/oyt_front_product
-git submodule add --force https://github.com/nestorsgarzonc/oyt_front_restaurant
-git submodule add --force https://github.com/nestorsgarzonc/oyt_front_table
-git submodule add --force https://github.com/nestorsgarzonc/oyt_front_widgets
+git submodule add https://github.com/nestorsgarzonc/oyt_front_auth
+git submodule add https://github.com/nestorsgarzonc/oyt_front_core
+git submodule add https://github.com/nestorsgarzonc/oyt_front_order
+git submodule add https://github.com/nestorsgarzonc/oyt_front_orders_queue
+git submodule add https://github.com/nestorsgarzonc/oyt_front_product
+git submodule add https://github.com/nestorsgarzonc/oyt_front_restaurant
+git submodule add https://github.com/nestorsgarzonc/oyt_front_table
+git submodule add https://github.com/nestorsgarzonc/oyt_front_widgets
 flutter pub get
