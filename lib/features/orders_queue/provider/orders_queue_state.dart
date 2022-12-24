1ce984eba6f0c43fa5d4adcbccec215407a30203
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-24 00:09:57 -0500

    Fix recomendations

--- lib/features/orders_queue/provider/orders_queue_state.dart
+++ lib/features/orders_queue/provider/orders_queue_state.dart
@@ -3,11 +3,11 @@ import 'package:oyt_front_core/wrappers/state_wrapper.dart';
 import 'package:oyt_front_orders_queue/models/orders_queue.dart';
 
 class OrdersQueueState extends Equatable {
-  final StateAsync<List<OrdersQueueModel>> ordersQueue;
-
-  const OrdersQueueState(this.ordersQueue);
 
   factory OrdersQueueState.initial() => OrdersQueueState(StateAsync.initial());
+
+  const OrdersQueueState(this.ordersQueue);
+  final StateAsync<List<OrdersQueueModel>> ordersQueue;
 
   @override
   List<Object?> get props => [ordersQueue];
